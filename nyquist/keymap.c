99350a4c7c7b9ba1b3caf7170189d3b8aeac8b67
Ryan Ford <ryanford@poluglottos.com>
2021-04-11 17:00:32 +0200

    playing around with more shortcuts

--- nyquist/keymap.c
+++ nyquist/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,               KC_1,    KC_2,    KC_3,                   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,                   KC_9,    KC_0,    KC_BSPC, \
   US_TILD,              KC_Q,    KC_W,    KC_E,                   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,                   KC_O,    KC_P,    KC_BSLS, \
   LT(_SPMOD, KC_TAB),   KC_A,    KC_S,    KC_D,                   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,                   KC_L,    KC_SCLN, US_QUOT, \
-  KC_LSFT,              KC_Z,    KC_X,    KC_C,                   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,                KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LSFT,              KC_Z,    KC_X,    KC_C,                   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,                KC_DOT,  KC_SLSH, KC_ENT, \
   KC_LCTL,              KC_LALT, KC_LGUI, LT(_MOUSEMOD, KC_LBRC), LOWER,   KC_SPC,  KC_SPC,  RAISE,   LT(_MOUSEMOD, KC_RBRC), KC_RGUI, KC_RALT, KC_RCTL \
 ),
 
@@ -47,19 +47,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |     |    \  |  |   |
+ * |      |      |      |      |Fullsc|      |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |      |      |      |      |      |      |      |      |      |      | Term |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | Prev |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,      KC_F10,  KC_F11,  KC_F12,  \
-  US_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______, \
-  _______, _______, _______, KC_MPRV, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY  \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+  US_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+  _______, _______, _______, _______, LGUI(KC_F), _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, LGUI(KC_ENT),  \
+  _______, _______, _______, KC_MPRV, _______,    _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Raise
@@ -106,19 +106,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  /* Space/Tab Modifier
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |      | WS1  | WS2  | WS3  | WS4  | WS5  | WS6  | WS7  | WS8  | WS9  | WS10 | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |Sleep |      |      |      |      |      |   Ü  |      |   Ö  |      | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  Ä   |  ß   | HOME | END  |      |      | LEFT |  UP  | DOWN | RIGHT|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      | TERM |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | Last |      |Albert|Albert|      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_SPMOD] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,      _______,       _______, _______, _______,  _______, KC_DEL,  \
+  _______, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),      LGUI(KC_6),       LGUI(KC_7), LGUI(KC_8), LGUI(KC_9),  LGUI(KC_0), KC_DEL,  \
   _______, KC_SLEP, _______, _______, _______, _______,      _______,       US_UDIA, _______, US_ODIA,  _______, KC_DEL,  \
   _______, US_ADIA, US_SS,   KC_HOME, KC_END,  _______,      KC_LEFT,       KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
   _______, _______, _______, _______, _______, _______,      _______,       _______, _______, _______,  _______, _______, \
